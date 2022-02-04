338613cc1b54ef7c85d763f5fd4cf0fdbe502b6e
Aarav Arora <aaravarora000@icloud.com>
2022-02-04 08:02:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fs  = require('fs');
 const {webss} = require("./utils");
 const {inline} = require("./utils")
 const keyboard = inline();
-bot.command(['start','help],
+bot.command(['start','help'],
     async (ctx) =>
     { await ctx.reply(
         `Hey [${ctx.message.from.first_name}](tg://user?id=${ctx.message.from.id}), I am a simple Bot to capture web screenshots,To capture one send /webss and your link`, {parse_mode: "MarkdownV2",reply_markup: keyboard, reply_to_message_id: ctx.msg.message_id}
