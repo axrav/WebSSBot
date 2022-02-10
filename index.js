a0fc8c7c8d66370b3f09d027b4a0f4d7cf1738c4
JDEVD <96882663+JDEVD@users.noreply.github.com>
2022-02-11 02:23:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const keyboard = inline();
 bot.command(['start','help'],
     async (ctx) =>
     { await ctx.reply(
-        `Hey [${ctx.message.from.first_name}](tg://user?id=${ctx.message.from.id}), I am a simple Bot to capture web screenshots,To capture one send /webss and your link`, {parse_mode: "MarkdownV2",reply_markup: keyboard, reply_to_message_id: ctx.msg.message_id}
+        `Hey [${ctx.message.from.first_name}](tg://user?id=${ctx.message.from.id}), I am a simple Bot to capture web screenshots,To capture one send /webss with your link`, {parse_mode: "MarkdownV2",reply_markup: keyboard, reply_to_message_id: ctx.msg.message_id}
 )})
 bot.command('webss', 
     async (ctx) =>
