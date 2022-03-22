19b0e15b822c9906f4ae0d2743d181cea93fcf74
Aarav Arora <aaravarora5600@gmail.com>
2022-03-23 01:58:13 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -6,7 +6,7 @@ const inline = () =>{
     .url("Owner", "https://t.me/VegetaxD")
     return inline_key; }
 const webss = async (link, user) => {
-    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const browser = await puppeteer.launch({executablePath: '/usr/bin/chromium-browser',args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     await page.goto(link);
     await page.screenshot({path: `${user}.png`});
